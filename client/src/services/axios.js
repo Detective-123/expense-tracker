1f1ee249e6e8c932f90c8927d5cb53e04d77af76
Gaurav Sawant <gauravsawant8217@gmail.com>
2026-03-31 18:19:41 +0530

    axios bug

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 console.log(import.meta.env.VITE_API_URL)
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "",
+  baseURL: "https://expense-tracker-3tkr.onrender.com" || "",
   withCredentials: true   
 })
 
