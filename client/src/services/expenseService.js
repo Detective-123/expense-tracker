22ec24ec448fd27309ee1b40ed5c674b8fac3e19
Gaurav Sawant <gauravsawant8217@gmail.com>
2026-03-31 17:53:56 +0530

    api bug

--- client/src/services/expenseService.js
+++ client/src/services/expenseService.js
@@ -1,18 +1,18 @@
 import api from "./axios.js";
 
 const createExpense = async (data) => {
-  const res = await api.post("api/v1/expense/add", data);
+  const res = await api.post("/api/v1/expense/add", data);
   return res.data;
 };
 
 const getExpense = async () => {
-  const res = await api.get("api/v1/expense/get");
+  const res = await api.get("/api/v1/expense/get");
   // console.log("Backend response: ", res.data);
   return res.data;
 };
 
 const deleteExpense = async (id) => {
-  const res = await api.delete(`api/v1/expense/${id}`)
+  const res = await api.delete(`/api/v1/expense/${id}`)
   return res.data;
 };
 
