ba4de4bca710f540108381059d7004c114d72f26
Gaurav Sawant <gauravsawant8217@gmail.com>
2026-03-31 17:53:45 +0530

    client bugs

--- client/src/services/authServices.js
+++ client/src/services/authServices.js
@@ -2,25 +2,25 @@ import api from "./axios.js";
 
 // register
 const register = async (data) => {
-  const res = await api.post("api/v1/auth/register", data);
+  const res = await api.post("/api/v1/auth/register", data);
   return res.data;
 };
 
 // login
 const login = async (data) => {
-  const res = await api.post("api/v1/auth/login", data);
+  const res = await api.post("/api/v1/auth/login", data);
   return res.data;
 };
 
 // get user
 const getUser = async () => {
-  const res = await api.get("api/v1/auth/current-user");
+  const res = await api.get("/api/v1/auth/current-user");
   return res.data;
 };
 
 // logout
 const logout = async () => {
-  const res = await api.post("api/v1/auth/logout");
+  const res = await api.post("/api/v1/auth/logout");
   return res.data;
 };
 export { register, login, getUser, logout };
